06dab584db29c1f9d17ad9042e90d08288af8e5f
Ganajr <Ganajr@gmail.com>
2025-02-07 17:35:00 +0000

    first commit

---
+++ src/test/java/sn/l3l2i/gestionEnseignants/service/EnseignantServiceTest.java
@@ -0,0 +1,66 @@
+package sn.l3l2i.gestionEnseignants.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sn.l3l2i.gestionEnseignants.models.Enseignant;
+import sn.l3l2i.gestionEnseignants.repository.EnseignantRepository;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EnseignantServiceTest {
+    @Mock
+    private EnseignantRepository enseignantRepository;
+    @InjectMocks
+    private EnseignantService enseignantService;
+
+    @Test
+    public void testAjouterEnseignants(){
+
+
+
+            Enseignant enseignant = new Enseignant(1L, "DIOUF", "Ngone", "dioufngone869@gmail.com");
+
+            when(enseignantRepository.save(any(Enseignant.class))).thenReturn(enseignant);
+
+            Enseignant savedEnseignant = enseignantService.ajouterEnseignant(enseignant);
+
+            assertNotNull(savedEnseignant);
+            assertEquals("DIOUF", savedEnseignant.getNom());
+            verify(enseignantRepository, times(1)).save(enseignant);
+        }
+
+    @Test
+    public void testTrouverEnseignantParId() {
+        Enseignant enseignant = new Enseignant(1L, "DIOUF", "Ngone", "dioufngone869@gmail.com");
+        when(enseignantRepository.findById(1L)).thenReturn(java.util.Optional.of(enseignant));
+
+        Enseignant foundEnseignant = enseignantService.trouverEnseignantParId(1L);
+
+        assertNotNull(foundEnseignant);
+        assertEquals("DIOUF", foundEnseignant.getNom());
+        verify(enseignantRepository, times(1)).findById(1L);
+    }
+    @Test
+    public void testTrouverEnseignantParId_NotFound() {
+        when(enseignantRepository.findById(1L)).thenReturn(java.util.Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> {
+            enseignantService.trouverEnseignantParId(1L);
+        });
+    }
+
+    @Test
+
+    public void testAjouterEnseignant_Null() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            enseignantService.ajouterEnseignant(null);
+        });
+    }
+}
